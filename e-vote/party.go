d785c2c6a26ee6dbe2e3302b562edae62f466b9c
ataberkozek <43850810+ataberkozek@users.noreply.github.com>
2021-07-01 15:47:44 +0300

    Update party.go

--- e-vote/party.go
+++ e-vote/party.go
@@ -186,3 +186,17 @@ func (s *SmartContract) GetAllPartys(ctx contractapi.TransactionContextInterface
 
 	return Partys, nil
 }
+
+func main() {
+
+	chaincode, err := contractapi.NewChaincode(new(SmartContract))
+
+	if err != nil {
+		fmt.Printf("Error create fabcar chaincode: %s", err.Error())
+		return
+	}
+
+	if err := chaincode.Start(); err != nil {
+		fmt.Printf("Error starting fabcar chaincode: %s", err.Error())
+	}
+}
